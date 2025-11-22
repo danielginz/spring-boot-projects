d33a06871271f87210df350c31eaafd4639cb4b2
danielginz <42783698+danielginz@users.noreply.github.com>
2025-11-22 20:54:01 +0200

    Add files via upload

---
+++ simple-docker/src/main/java/com/docker/test/simple_docker/SimpleDockerApplication.java
@@ -0,0 +1,37 @@
+package com.docker.test.simple_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+import java.util.Random;
+
+@SpringBootApplication
+public class SimpleDockerApplication {
+	public static final List<String> WORDS = List.of(
+			"AAA",
+			"BBB",
+			"CCC",
+			"DDD",
+			"EEE"
+	);
+
+	private static final Random RANDOM = new Random();
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleDockerApplication.class, args);
+
+		while (true) {
+			String randomPhrase = WORDS.get(RANDOM.nextInt(WORDS.size()));
+			System.out.println(randomPhrase);
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
+}
+
+//https://www.youtube.com/watch?v=0oke9VfRt2M
---
+++ simple-docker/src/test/java/com/docker/test/simple_docker/SimpleDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker.test.simple_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
